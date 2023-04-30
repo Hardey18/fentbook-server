3f9cdcdd27432458dd0d85b985137d29b5a3dff5
Nurudeen Adewale <35535871+Hardey18@users.noreply.github.com>
2023-04-30 23:24:51 +0100

    Added any types

--- src/services/nodemailer.ts
+++ src/services/nodemailer.ts
@@ -14,7 +14,7 @@ const transport = nodemailer.createTransport({
 export = {
   sendEmail(from: string, to: string, subject: string, html: string): Promise<unknown> {
     return new Promise((resolve, reject) => {
-      transport.sendMail({ from, subject, to, html }, (err, info) => {
+      transport.sendMail({ from, subject, to, html }, (err: any, info: any) => {
         if (err) reject(err);
         resolve(info);
       });
